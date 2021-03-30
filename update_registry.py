3d07b7c76eff907e89f198237b1c50ec3805130c
David Ory <david.ory@gmail.com>
2021-03-30 19:33:21 -0400

    update python for model_link_id
    #3

--- update_registry.py
+++ update_registry.py
@@ -5,7 +5,7 @@ from network_wrangler import ProjectCard
 import pandas as pd
 
 card_dir = os.path.join('.', 'projects')
-filename_node = 'node_registry.csv'
+filename_registry = 'registry.csv'
 filename_config = 'registry_config.yml'
 
 card_filenames = []
@@ -16,9 +16,18 @@ for (dirpath, dirnames, filenames) in os.walk(card_dir):
             card_filenames.append(os.path.join(card_dir, filename))
     break
 
-node_df = pd.read_csv(filename_node)
+registry_df = pd.read_csv(filename_registry)
+node_df = registry_df[registry_df['node_or_link'] == 'node']
+link_df = registry_df[registry_df['node_or_link'] == 'link']
+
 with open(filename_config, "r") as config_file:
     config_dict = yaml.safe_load(config_file)
+
+for card_filename in card_filenames:
+    write_updated_card = False
+    card = ProjectCard.read(card_filename, validate=False)
+    card_dict = card.__dict__
+    card_index = 0
 
 for card_filename in card_filenames:
     write_updated_card = False
@@ -29,40 +38,87 @@ for card_filename in card_filenames:
         node_index = 0
         for node in card_dict['nodes']:
             new_node = node['model_node_id']
-
             if (new_node <= config_dict['start_node_number']):
-                msg = "New node number ({}) in project '{}' is less than'\
-                'the starting node number in config file of {}".format(
-                    new_node,
-                    card_dict['project'],
-                    config_dict['start_node_number'],
-                )
+                msg = "New node number ({}) in project '{}' is less than the \
+                 starting node number in config file of {}".format(
+                        new_node,
+                        card_dict['project'],
+                        config_dict['start_node_number'],
+                    )
                 raise ValueError(msg)
 
-            if (new_node not in node_df['node'].values):
+            if (new_node not in node_df['number'].values):
                 df = pd.DataFrame(
-                    {'node': [new_node], 'project': [card_dict['project']]}
+                    {
+                        'node_or_link': 'node',
+                        'number': [new_node],
+                        'project': [card_dict['project']],
+                    }
                 )
                 node_df = node_df.append(df)
             else:
-                number = 1 + node_df['node'].max()
+                number = 1 + node_df['number'].max()
                 card_dict['nodes'][node_index]['model_node_id'] = number
                 for i in range(0, len(card_dict['links'])):
                     if (card_dict['links'][i]['A'] == new_node):
                         card_dict['links'][i]['A'] = number
                     if (card_dict['links'][i]['B'] == new_node):
                         card_dict['links'][i]['B'] = number
-
                 df = pd.DataFrame(
-                    {'node': [number], 'project': [card_dict['project']]}
+                    {
+                        'node_or_link': 'node',
+                        'number': [number],
+                        'project': [card_dict['project']],
+                    }
                 )
                 node_df = node_df.append(df)
                 write_updated_card = True
 
             node_index = node_index + 1
 
+        link_index = 0
+        for link in card_dict['links']:
+            new_link = link['model_link_id']
+
+            if (new_link <= config_dict['start_link_number']):
+                msg = "New link id ({}) in project '{}' is less than the \
+                starting link number in config file of {}".format(
+                        new_link,
+                        card_dict['project'],
+                        config_dict['start_link_number'],
+                    )
+                raise ValueError(msg)
+
+            if (new_link not in link_df['number'].values):
+                df = pd.DataFrame(
+                    {
+                        'node_or_link': 'link',
+                        'number': [new_link],
+                        'project': [card_dict['project']],
+                    }
+                )
+                link_df = link_df.append(df)
+            else:
+                number = 1 + link_df['number'].max()
+                card_dict['links'][link_index]['model_link_id'] = number
+                for i in range(0, len(card_dict['links'])):
+                    if (card_dict['links'][i]['model_link_id'] == new_link):
+                        card_dict['links'][i]['model_link_id'] = number
+                df = pd.DataFrame(
+                    {
+                        'node_or_link': 'link',
+                        'number': [number],
+                        'project': [card_dict['project']],
+                    }
+                )
+                link_df = link_df.append(df)
+                write_updated_card = True
+
+            link_index = link_index + 1
+
     if (write_updated_card):
         card.__dict__.update(card_dict)
         card.write(filename=card_filename)
 
-node_df.to_csv(filename_node, index=False)
+out_df = node_df.append(link_df)
+out_df.to_csv(filename_registry, index=False)
