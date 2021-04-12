b18ea5c1f8c5e83d76d2032e9b3b228ac41d3b79
David Ory <david.ory@gmail.com>
2021-04-12 15:28:39 -0400

    fix github branch, add either or update to test

--- tests/test_update_registry.py
+++ tests/test_update_registry.py
@@ -39,13 +39,27 @@ def test_update_registry(request):
         ["link", 501, "Project B"],
         ["link", 502, "Project A"],
     ]
-    target_df = pd.DataFrame(data, columns=["type", "id", "project_added"])
-    target_df = target_df.sort_values(by=["type", "id"]).reset_index(drop=True)
+    target_i_df = pd.DataFrame(data, columns=["type", "id", "project_added"])
+    target_i_df = target_i_df.sort_values(by=["type", "id"]).reset_index(drop=True)
+
+    data = [
+        ["node", 1001, "Project A"],
+        ["node", 1002, "Project A"],
+        ["node", 1003, "Project B"],
+        ["node", 1004, "Project B"],
+        ["link", 501, "Project A"],
+        ["link", 502, "Project B"],
+    ]
+    target_ii_df = pd.DataFrame(data, columns=["type", "id", "project_added"])
+    target_ii_df = target_ii_df.sort_values(by=["type", "id"]).reset_index(drop=True)
 
     outcome_df = pd.read_csv(output_file)
     outcome_df = outcome_df.sort_values(by=["type", "id"]).reset_index(drop=True)
 
-    assert target_df.equals(outcome_df) is True
+    assert (
+        target_i_df.equals(outcome_df) is True
+        or target_ii_df.equals(outcome_df) is True
+    )
 
 
 @pytest.mark.ci
@@ -83,10 +97,27 @@ def test_update_registry_existing(request):
         ["link", 502, "Project B"],
         ["link", 503, "Project A"],
     ]
-    target_df = pd.DataFrame(data, columns=["type", "id", "project_added"])
-    target_df = target_df.sort_values(by=["type", "id"]).reset_index(drop=True)
+    target_i_df = pd.DataFrame(data, columns=["type", "id", "project_added"])
+    target_i_df = target_i_df.sort_values(by=["type", "id"]).reset_index(drop=True)
+
+    data = [
+        ["node", 1001, "Project Z"],
+        ["node", 1002, "Project Z"],
+        ["link", 501, "Project Z"],
+        ["node", 1003, "Project A"],
+        ["node", 1004, "Project A"],
+        ["node", 1005, "Project B"],
+        ["node", 1006, "Project B"],
+        ["link", 502, "Project A"],
+        ["link", 503, "Project B"],
+    ]
+    target_ii_df = pd.DataFrame(data, columns=["type", "id", "project_added"])
+    target_ii_df = target_ii_df.sort_values(by=["type", "id"]).reset_index(drop=True)
 
     outcome_df = pd.read_csv(output_file)
     outcome_df = outcome_df.sort_values(by=["type", "id"]).reset_index(drop=True)
 
-    assert target_df.equals(outcome_df) is True
+    assert (
+        target_i_df.equals(outcome_df) is True
+        or target_ii_df.equals(outcome_df) is True
+    )
